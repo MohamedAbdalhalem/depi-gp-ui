2710870e2892fd4159569c270aaa2a849f464afa
MohamedAbdalhalem <mohamed3ab7alem@gmail.com>
2026-04-15 05:35:43 +0200

    fix a bug in import ProuctsLoadingScreen component

--- src/components/productList.jsx
+++ src/components/productList.jsx
@@ -1,11 +1,11 @@
 import Product from "./Product";
-import ProductsLodaingScreen from "./productsLodaingScreen";
 import { memo } from "react";
+import ProductsLodaingScreen from './ProductsLodaingScreen';
 
 export default memo(function ProductList({ allProduct, isLoading }) {
   return (
     <>
-      {isLoading && <ProductsLodaingScreen />}
+      {isLoading && <ProductsLodaingScreen/>}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-8">
         {allProduct?.map((product) => (
           <Product
